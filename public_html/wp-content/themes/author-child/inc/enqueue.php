465ca5e1016698159c1770df79ddeedbcf5ad5cd
John Merigliano <jmerigliano@gmail.com>
2020-06-24 16:26:48 -0400

    swapped in new fonts and footer color
    added versionin var to the enqueue script

--- public_html/wp-content/themes/author-child/inc/enqueue.php
+++ public_html/wp-content/themes/author-child/inc/enqueue.php
@@ -1,15 +1,22 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'author_child_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'author_child_enqueue_styles');
 function author_child_enqueue_styles() {
     $parenthandle = 'parent-style';
     $theme = wp_get_theme();
-    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css',
+
+    if (ENV === 'development') {
+        $ver = random_int(111, 99999); // dev only
+    } else {
+        $ver = '2020-06-24';
+    }
+
+    wp_enqueue_style($parenthandle, get_template_directory_uri() . '/style.css',
         array(),
         $theme->parent()->get('Version')
     );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( $parenthandle ),
-        $theme->get('Version')
+    wp_enqueue_style('child-style', get_stylesheet_uri(),
+        array($parenthandle),
+        $ver
     );
 }
