52dc62ef5036b999745399b65bd2cfb449719df9
John Merigliano <jmerigliano@gmail.com>
2020-07-19 12:53:19 -0400

    removed author name from meta, as it's redundant and did not lead to an author archive. removed hover underline from h2 entry titles as it's not necessary

--- public_html/wp-content/themes/markiter-child/partials/meta.php
+++ public_html/wp-content/themes/markiter-child/partials/meta.php
@@ -7,37 +7,25 @@
 
 ?>
 <div class="entry-meta">
-	<span class="entry-author">
-		<?php
-		echo wp_kses_post(
-			sprintf(
-				// translators: %s is for the author link.
-				__( 'by %s', 'markiter' ),
-				get_the_author_link()
-			)
-		);
-		?>
-	</span>
-
-	<span class="entry-date">
-		<?php
-		echo wp_kses(
-			sprintf(
-				// translators: %s is for the author link.
-				__( '%s', 'markiter' ),
-				markiter_get_the_date()
-			),
-			array(
-				'a' => array(
-					'href' => array(),
-					'rel'  => array(),
-				),
-				'time' => array(
-					'datetime' => array(),
-					'class'    => array(),
-				),
-			)
-		);
-		?>
-	</span>
+    <span class="entry-date">
+        <?php
+        echo wp_kses(
+            sprintf(
+                // translators: %s is for the author link.
+                __( '%s', 'markiter' ),
+                markiter_get_the_date()
+            ),
+            array(
+                'a' => array(
+                    'href' => array(),
+                    'rel'  => array(),
+                ),
+                'time' => array(
+                    'datetime' => array(),
+                    'class'    => array(),
+                ),
+            )
+        );
+        ?>
+    </span>
 </div>
